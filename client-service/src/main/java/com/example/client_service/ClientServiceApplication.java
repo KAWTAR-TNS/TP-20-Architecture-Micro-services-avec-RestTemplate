08023ad78b60cf2e2fe44c48738cc73018b17e3f
kawtar-tns <tanassakawtar@gmail.com>
2025-12-07 15:20:07 +0100

    second

--- client-service/src/main/java/com/example/client_service/ClientServiceApplication.java
+++ client-service/src/main/java/com/example/client_service/ClientServiceApplication.java
@@ -2,10 +2,11 @@ package com.example.client_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
-@EnableEurekaClient  // Active l'enregistrement auprès d'Eureka
+@EnableDiscoveryClient
 public class ClientServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClientServiceApplication.class, args);
