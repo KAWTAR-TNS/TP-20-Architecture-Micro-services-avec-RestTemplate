594a8fab1df79742d554c7c40ce78e55b48522d1
kawtar-tns <tanassakawtar@gmail.com>
2025-12-07 18:04:35 +0100

    Ca7

--- Car-service/src/main/java/com/example/Car_service/repositories/CarRepository.java
+++ Car-service/src/main/java/com/example/Car_service/repositories/CarRepository.java
@@ -1,4 +1,13 @@
 package com.example.Car_service.repositories;
 
-public class CarRepository {
+import com.example.Car_service.entities.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long> {
+    // Méthodes CRUD de base fournies par JpaRepository
+
+    // Vous pouvez ajouter des méthodes personnalisées ici, par exemple:
+    // List<Car> findByClientId(Long clientId);
 }
